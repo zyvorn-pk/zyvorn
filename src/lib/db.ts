f27ebc69bbe69659fe15454ba974c76a4563d0c5
Ali Hamas <alihamasdev@gmail.com>
2025-11-22 01:43:56 +0500

    chore: update to prisma@7.0.0

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,14 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+
 import { PrismaClient } from "@/lib/generated/prisma/client";
+
+const adapter = new PrismaPg({
+	connectionString: process.env.DATABASE_URL
+});
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-const prisma = globalForPrisma.prisma || new PrismaClient();
+const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
